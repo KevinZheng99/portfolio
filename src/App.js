95b27fd19779596ca4ecb25d53e53fa2ee0321e1
KevinZheng99 <kevinzhengkz99@gmail.com>
2021-11-28 22:34:17 -0500

    Navigation and Hero structure/layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Fragment } from "react";
 
 import Navigation from "./components/Navigation/Navigation";
+import Hero from "./components/Hero/Hero";
 
 function App() {
   return (
     <Fragment>
       <Navigation />
+      <Hero />
     </Fragment>
   );
 }
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,5 +1,21 @@
 import React from "react";
 
+import classes from "./Navigation.module.css";
+
 export default function Navigation() {
-  return <div></div>;
+  return (
+    <div className={classes.navigation}>
+      <a className={classes.logo} href="#">
+        KZ
+      </a>
+
+      <div>
+        <a href="#">About</a>
+        <a href="#">Work</a>
+        <a className={classes.resume} href="#">
+          Resume
+        </a>
+      </div>
+    </div>
+  );
 }
